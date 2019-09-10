78626cf4bedc8f95e868f2bf6097b84848ae476a
Neha Patel <neha.r.p@gmail.com>
2019-09-10 14:53:24 -0700

    filling out schema for create-cars table

--- data/migrations/20190910144946_create-cars.js
+++ data/migrations/20190910144946_create-cars.js
@@ -1,6 +1,12 @@
 
 exports.up = function(knex) {
-  
+  //create table called cars
+  return knex.schema.createTable('cars', tbl => {
+      //id, primary key, auto-increment, integer
+      tbl.icrements();
+
+
+  })
 };
 
 exports.down = function(knex) {
